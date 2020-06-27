18f84688167fcedbd9c29f0b7e15abfe53c91a87
Laura Shears <52978933+shearsl@users.noreply.github.com>
2020-06-27 13:33:32 -0400

    Server built

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Lsquared Math app works.')
+import express from 'express';
+import { homePageStr } from './indexWebPage';
+
+const app = express();
+
+app.use('/', (req, res)=>{
+    res.send(homePageStr);
+});
+
+app.listen(2006, ()=>{
+    console.log('Server has started.');
+});
+
---
+++ src/indexWebPage.ts
@@ -0,0 +1,9 @@
+let title:String = `Lsquared Math Employee Expense Reimbursement`;
+export let homePageStr:string = "";
+homePageStr += `<!DOCTYPE html>\n`;
+homePageStr += `<html lang="en">\n`;
+homePageStr += `<head><title>${title}</title></head>\n`;
+homePageStr += `<body style="background-color: #33ffff;">\n`;
+homePageStr += `<div style="width:80%; margin:auto; background-color:#cc9999; border:3px solid #336666; padding:0 15px 0 15px;">`;
+homePageStr += `<h1 style="font-size: 1.3em;">${title}</h1>\n`;
+homePageStr += `</div></body>\n</html>`;
