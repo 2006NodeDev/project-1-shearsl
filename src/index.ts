511e22ae768abc82c1786d3bae1f28c47a23a17c
Laura Shears <52978933+shearsl@users.noreply.github.com>
2020-06-27 22:38:48 -0400

    added code similar to week 1 day 4

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,135 @@
-import express from 'express';
-import { homePageStr } from './indexWebPage';
+import e, { Request, Response } from 'express';
+//import { homePageStrStart } from './indexWebPage';
+import { User } from './models/User';
 
-const app = express();
+const app = e(); //call the express function and get a completed application
 
-app.use('/', (req, res)=>{
-    res.send(homePageStr);
+//app .use matches every single http verb (get, post, and delete)
+//if I don't specify a path, that's the same as every path
+app.use(e.json()); //this is an example of middleware
+//takes the request and turns it into a json object and then lets it pass to the next request
+
+app.get('/users', (req:Request, res:Response)=>{
+    res.json(users);
 });
+
+app.post('/users', (req:Request, res:Response)=>{
+     console.log(req.body);
+     let {userId, username, password, firstName, lastName, email, role} = req.body
+     if(userId && username && password && firstName && lastName && email && role){
+         users.push({userId, username, password, firstName, lastName, email, role});
+         res.sendStatus(201)
+     }else{
+         res.status(400).send("Please enter all required information.");
+        //throw new UserInputError();
+     }
+     res.sendStatus(501);
+})
 
 app.listen(2006, ()=>{
     console.log('Server has started.');
 });
 
+/*app.use('/', (req, res)=>{
+    res.send(homePageStrStart);
+    //res.send(homePageStrEnd);
+}); */
+
+let users:User[] = [
+    {
+        userId: 101,
+        username: "lauras",
+        password: "password",
+        firstName: "Laura",
+        lastName: "Fakelaname",
+        email: "lauras@fakemail.com",
+        role: [{
+            roleId: 1, 
+            role: "owner,developer,mathematician"
+        }]
+    },
+    {
+        userId: 102,
+        username: "emmas",
+        password: "password",
+        firstName: "Emma",
+        lastName: "Fakelaname",
+        email: "emmas@fakemail.com",
+        role: [{
+            roleId: 3, 
+            role: "developer,sales"
+        }]
+    },
+    {
+        userId: 103,
+        username: "joboah",
+        password: "password",
+        firstName: "Joboa",
+        lastName: "Fakelaname",
+        email: "joboah@fakemail.com",
+        role: [{
+            roleId: 2, 
+            role: "networkAdmin,developer"
+        }]
+    },
+    {
+        userId: 104,
+        username: "rachelr",
+        password: "password",
+        firstName: "Rachel",
+        lastName: "Fakelaname",
+        email: "rachelr@fakemail.com",
+        role: [{
+            roleId: 3, 
+            role: "developer, mathematician"
+        }]
+    },
+    {
+        userId: 105,
+        username: "firdevsy",
+        password: "password",
+        firstName: "Firdevs",
+        lastName: "Fakelaname",
+        email: "firdevsy@fakemail.com",
+        role: [{
+            roleId: 3, 
+            role: "developer,tester"
+        }]
+    },
+    {
+        userId: 106,
+        username: "taniquea",
+        password: "password",
+        firstName: "Tanique",
+        lastName: "Fakelaname",
+        email: "taniquea@fakemail.com",
+        role: [{
+            roleId: 3, 
+            role: "developer,writer"
+        }]
+    },
+    {
+        userId: 107,
+        username: "rebap",
+        password: "password",
+        firstName: "Reba",
+        lastName: "Fakelaname",
+        email: "rebap@fakemail.com",
+        role: [{
+            roleId: 1, 
+            role: "developer,human resources"
+        }]
+    },
+    {
+        userId: 108,
+        username: "chrishp",
+        password: "password",
+        firstName: "Chrish",
+        lastName: "Fakelaname",
+        email: "chrishp@fakemail.com",
+        role: [{
+            roleId: 3, 
+            role: "developer,writer"
+        }]
+    }
+]
--- src/indexWebPage.ts
+++ src/indexWebPage.ts
@@ -1,9 +1,11 @@
-let title:String = `Lsquared Math Employee Expense Reimbursement`;
-export let homePageStr:string = "";
-homePageStr += `<!DOCTYPE html>\n`;
-homePageStr += `<html lang="en">\n`;
-homePageStr += `<head><title>${title}</title></head>\n`;
-homePageStr += `<body style="background-color: #33ffff;">\n`;
-homePageStr += `<div style="width:80%; margin:auto; background-color:#cc9999; border:3px solid #336666; padding:0 15px 0 15px;">`;
-homePageStr += `<h1 style="font-size: 1.3em;">${title}</h1>\n`;
-homePageStr += `</div></body>\n</html>`;
+let title:String = `Lsquared Math Employee Expense Reimbursement System`;
+export let homePageStrStart:string = "";
+homePageStrStart += `<!DOCTYPE html>\n`;
+homePageStrStart += `<html lang="en">\n`;
+homePageStrStart += `<head>\n<title>${title}</title>\n<meta charset="utf-8"></head>\n`;
+homePageStrStart += `<body style="background-color: #0036f4; font-family: Arial, sans-serif;">\n`;
+homePageStrStart += `<div style="width:80%; margin:auto; background-color:#003600; text-align:center; border:3px solid #bababa; padding:0 15px 0 15px;">`;
+homePageStrStart += `<h1 style="font-size: 1.3em; color:#bababa; margin:auto;">${title}</h1>\n`;
+
+
+export let homePageStrEnd = `</div></body>\n</html>`;
